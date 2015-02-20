c2574d21ab28bc98f773c3744a5f424b486b4c2f
alariel91 <monysaenzcarrero@gmail.com>
2015-02-20 10:26:45 +0100

    Crear las notas de un alumno pero ahora usando JPA Tools para generar los VO

--- src/main/java/es/opensigad/model/base/Principal.java
+++ src/main/java/es/opensigad/model/base/Principal.java
@@ -2,19 +2,39 @@ package es.opensigad.model.base;
 
 import javax.persistence.EntityManager;
 
+import es.opensigad.model.vo.Alumno;
+import es.opensigad.model.vo.AlumnoMatricula;
 import es.opensigad.model.vo.AlumnoNota;
+import es.opensigad.model.vo.EnsenanzaMateria;
 
 public class Principal {
 
 	public static void main(String[] args) {
-
+		
+		// --------- Alumno Matricula ----------//
+		AlumnoMatricula alumnoMatricula =new AlumnoMatricula();
+		alumnoMatricula.setId(1);
+		
+		// --------- Enseñanza Materia ----------//
+		EnsenanzaMateria ensenanzaMateria=new EnsenanzaMateria();
+		ensenanzaMateria.setId(1);
+		
+		// --------- Alumno ----------//
+		Alumno al=new Alumno();
+		al.setId(1);
+		al.setSexo("h");
+		
+		// --------- Alumno Nota ----------//
 		AlumnoNota alumno = new AlumnoNota();
-		alumno.setEvaluacion('1');
-		alumno.setNota(4);
-		alumno.setObservacion("Suspenso");
+		alumno.setAlumnoMatricula(alumnoMatricula);
+		alumno.setEnsenanzaMateria(ensenanzaMateria);
+		alumno.setEvaluacion("1");
+		alumno.setNota(10);
+		alumno.setObservacion("¡¡Perfecto un 10!!");
 
 		EntityManager em = PersistenceManager.INSTANCE.getEntityManager();
 		em.getTransaction().begin();
+		em.persist(al);
 		em.persist(alumno);
 		em.getTransaction().commit();
 		em.close();
--- src/main/java/es/opensigad/model/vo/AlumnoNota.java
+++ src/main/java/es/opensigad/model/vo/AlumnoNota.java
@@ -1,35 +1,63 @@
 package es.opensigad.model.vo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import java.io.Serializable;
+import javax.persistence.*;
 
+
+/**
+ * The persistent class for the alumno_nota database table.
+ * 
+ */
 @Entity
-@Table(name = "alumno_nota")
-public class AlumnoNota {
-	@Id
-	@GeneratedValue
-	private Integer id;
+@Table(name="alumno_nota")
+@NamedQuery(name="AlumnoNota.findAll", query="SELECT a FROM AlumnoNota a")
+public class AlumnoNota implements Serializable {
+	private static final long serialVersionUID = 1L;
 
-	private char evaluacion;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(unique=true, nullable=false)
+	private int id;
+
+	@Column(length=1)
+	private String evaluacion;
+
 	private int nota;
+
+	@Column(length=255)
 	private String observacion;
 
-	public Integer getId() {
-		return id;
+	//bi-directional many-to-one association to EnsenanzaMateria
+	@ManyToOne
+	@JoinColumn(name="id_materia")
+	private EnsenanzaMateria ensenanzaMateria;
+
+	//bi-directional many-to-one association to AlumnoMatricula
+	@ManyToOne
+	@JoinColumn(name="id_alumno_matricula")
+	private AlumnoMatricula alumnoMatricula;
+
+	public AlumnoNota() {
 	}
 
-	public void setId(Integer id) {
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
 		this.id = id;
 	}
 
-	public char getEvaluacion() {
-		return evaluacion;
+	public String getEvaluacion() {
+		return this.evaluacion;
+	}
+
+	public void setEvaluacion(String evaluacion) {
+		this.evaluacion = evaluacion;
 	}
 
 	public int getNota() {
-		return nota;
+		return this.nota;
 	}
 
 	public void setNota(int nota) {
@@ -37,15 +65,27 @@ public class AlumnoNota {
 	}
 
 	public String getObservacion() {
-		return observacion;
+		return this.observacion;
 	}
 
 	public void setObservacion(String observacion) {
 		this.observacion = observacion;
 	}
 
-	public void setEvaluacion(char evaluacion) {
-		this.evaluacion = evaluacion;
+	public EnsenanzaMateria getEnsenanzaMateria() {
+		return this.ensenanzaMateria;
+	}
+
+	public void setEnsenanzaMateria(EnsenanzaMateria ensenanzaMateria) {
+		this.ensenanzaMateria = ensenanzaMateria;
+	}
+
+	public AlumnoMatricula getAlumnoMatricula() {
+		return this.alumnoMatricula;
+	}
+
+	public void setAlumnoMatricula(AlumnoMatricula alumnoMatricula) {
+		this.alumnoMatricula = alumnoMatricula;
 	}
 
 }
